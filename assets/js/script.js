53197632bbf8467934db9bf8f588466578c2b7ee
Herson Schluter <furyofthekillers@outlook.com>
2019-04-14 08:49:03 -0300

    analytics

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+const images = [
+    'fury-skull-jason.png', 
+    'fury-skull-punisher.png',
+    'fury-skull-rambo.png',
+    'fury-skull-mage.png',
+    'fury-skull-spawn.png',
+    'fury-skull-blade.png',
+    'fury-skull-coringa.png',
+    'fury-skull-desmond.png',
+    'fury-skull-johnwick.png',
+    'fury-skull-link.png',
+    'fury-skull-medic.png',
+    'fury-skull-mengele.png',
+    'fury-skull-sniper.png',
+    'fury-skull-skeletor.png',
+    'fury-skull-warmachine.png',
+    'fury-skull-terminator.png',
+    'fury-skull-base.png'
+]
+
 $(document).ready(function(){
     random();
     $('.brasao').on('click', function(){
@@ -6,25 +26,5 @@ $(document).ready(function(){
 });
 
 function random(){
-    var images = [
-        'fury-skull-jason.png', 
-        'fury-skull-punisher.png',
-        'fury-skull-rambo.png',
-        'fury-skull-mage.png',
-        'fury-skull-spawn.png',
-        'fury-skull-blade.png',
-        'fury-skull-coringa.png',
-        'fury-skull-desmond.png',
-        'fury-skull-johnwick.png',
-        'fury-skull-link.png',
-        'fury-skull-medic.png',
-        'fury-skull-mengele.png',
-        'fury-skull-sniper.png',
-        'fury-skull-skeletor.png',
-        'fury-skull-warmachine.png',
-        'fury-skull-terminator.png',
-        'fury-skull-base.png'
-    ] 
-
     $('.brasao').html('<img class="logo" src="assets/images/skulls/' + images[Math.floor(Math.random() * images.length)] + '" alt="Skull Fury of the Killers">');
 }
