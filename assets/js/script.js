dadd5693b2413887288769cb2b181ecd37589704
Herson Schluter <furyofthekillers@outlook.com>
2019-04-14 15:14:28 -0300

    teste de json members

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,9 +22,38 @@ $(document).ready(function(){
     random();
     $('.brasao').on('click', function(){
         random();
-    })
+    });    
 });
 
 function random(){
     $('.brasao').html('<img class="logo" src="assets/images/skulls/' + images[Math.floor(Math.random() * images.length)] + '" alt="Skull Fury of the Killers">');
 }
+
+
+
+var members = $.getJSON( "assets/json/members.json", function() {
+    console.log( "success" );
+}).done(function( data ) {
+    $.each( data.items, function( i, item ) {
+        tag     = item[i].gametag;
+        name    = item[i].name;
+        date    = item[i].date;
+        patent  = item[i].patent;
+        skull   = item[i].skull;
+        line    = item[i].line;
+        console.log('Gametag: '+tag+' | Nome: '+name+' | Nasc: '+date+' | Patente: '+patent+' | Caveira: '+skull+' | Linha: '+line);
+    });
+})
+.fail(function() {
+    console.log( "error" );
+})
+.always(function() {
+    console.log( "complete" );
+});
+
+// Perform other work here ...
+
+// Set another completion function for the request above
+members.complete(function() {
+    console.log( "second complete" );
+});
