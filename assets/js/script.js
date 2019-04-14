fef6cb2e3c8b7167b478d93858d13f9849da00fa
Herson Schluter <furyofthekillers@outlook.com>
2019-04-14 16:07:21 -0300

    ajuste javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 const images = [
-    'fury-skull-jason.png', 
-    'fury-skull-punisher.png',
-    'fury-skull-rambo.png',
-    'fury-skull-mage.png',
-    'fury-skull-spawn.png',
-    'fury-skull-blade.png',
-    'fury-skull-coringa.png',
-    'fury-skull-desmond.png',
-    'fury-skull-johnwick.png',
-    'fury-skull-link.png',
-    'fury-skull-medic.png',
-    'fury-skull-mengele.png',
-    'fury-skull-sniper.png',
-    'fury-skull-skeletor.png',
-    'fury-skull-warmachine.png',
-    'fury-skull-terminator.png',
-    'fury-skull-base.png'
+    'jason', 
+    'punisher',
+    'rambo',
+    'mage',
+    'spawn',
+    'blade',
+    'coringa',
+    'desmond',
+    'johnwick',
+    'link',
+    'medic',
+    'mengele',
+    'sniper',
+    'skeletor',
+    'warmachine',
+    'terminator',
+    'base'
 ]
 
 $(document).ready(function(){
@@ -26,70 +26,5 @@ $(document).ready(function(){
 });
 
 function random(){
-    $('.brasao').html('<img class="logo" src="assets/images/skulls/' + images[Math.floor(Math.random() * images.length)] + '" alt="Skull Fury of the Killers">');
+    $('.brasao').html('<img class="logo" src="assets/images/skulls/fury-skull-'+images[Math.floor(Math.random() * images.length)]+'.png" alt="Skull Fury of the Killers">');
 }
-
-
-
-var members = $.getJSON( "assets/json/members.json", function(data) {
-    console.log('data: '+data);
-    $.each( data.members, function( i, member ) {
-        console.log('data members: '+data.members);
-        console.log('member: '+member);
-        tag     = member[i].gametag;
-        name    = member[i].name;
-        date    = member[i].date;
-        patent  = member[i].patent;
-        skull   = member[i].skull;
-        line    = member[i].line;
-        console.log('Gametag: '+tag+' | Nome: '+name+' | Nasc: '+date+' | Patente: '+patent+' | Caveira: '+skull+' | Linha: '+line);
-    });
-    console.log( "success" );
-}).done(function() {
-    console.log( "done" );    
-})
-.fail(function() {
-    console.log( "error" );
-})
-.always(function() {
-    console.log( "complete" );
-});
-
-// function carregarItens(){
-//     //variáveis
-//     var url = "assets/json/members.json";
-
-//     //Capturar Dados Usando Método AJAX do jQuery
-//     $.ajax({
-//         type: "GET", 
-//         url: url,
-//         timeout: 3000,
-//         datatype: 'JSON',
-//         contentType: "application/json; charset=utf-8",
-//         cache: false,
-//         beforeSend: function() {
-//             console.log("Carregando..."); //Carregando
-//         },
-//         error: function() {
-//             console.log("O servidor não conseguiu processar o pedido");
-//         },
-//         success: function(members) {
-//             // Interpretando retorno JSON...
-//             var items = members;
-
-//             // Listando cada cliente encontrado na lista...
-//             $.each(items,function(i, member){
-//                 tag     = member[i].gametag;
-//                 name    = member[i].name;
-//                 date    = member[i].date;
-//                 patent  = member[i].patent;
-//                 skull   = member[i].skull;
-//                 line    = member[i].line;
-                
-//                 console.log('Gametag: '+tag+' | Nome: '+name+' | Nasc: '+date+' | Patente: '+patent+' | Caveira: '+skull+' | Linha: '+line);
-//             });
-//                 //Limpar Status de Carregando 
-//             console.log("Carregado"); 
-//         } 
-//     });  
-// }
