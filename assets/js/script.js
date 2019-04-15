cfa38a7a2c5a8b891009b9ab0bf341f5eb2747fb
Herson Schluter <herson.schluter@telefonica.com>
2019-04-15 16:48:51 -0300

    ajustes de script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var _listMembers = [];
-var _skulls = [];
+var _skulls     = [];
+var _line       = [];
 
 $(document).ready(function(){
     $('.brasao').on('click', function(){
@@ -20,12 +21,11 @@ function carregarMembros(){
             console.log("O servidor não conseguiu carregar os membros");
         },
         success: function(data) {
-            $.each(data,function(i, members){
-                $.each(members,function(i, member) {
-                    _listMembers.push(member);
-                });
+            $.each(data.members,function(i, member) {
+                _listMembers.push(member);
             });
             listSkulls(_listMembers);
+            // lineMembers(_listMembers);
             return _listMembers;
         }
     });
@@ -35,6 +35,20 @@ function listSkulls(data){
         _skulls.push(member.skull);
     });
     return _skulls;
+}
+function lineMembers(data){
+    var lineComp = ".competitive";
+    var lineCasu = ".casual";
+    $.each(data, function( i, member ) {
+        if(member.line == 'competitive'){
+            $(lineComp).append('<li><h2>'+member.gametag+'</h2><br><span>'+member.name+'</span><br><span>'+member.date+'</span></li>');
+        }
+    });
+    $.each(data, function( i, member ) {
+        if(member.line == 'casual'){
+            $(lineCasu).append('<li><h2>'+member.gametag+'</h2><br><span>'+member.name+'</span><br><span>'+member.date+'</span></li>');
+        }
+    });
 }
 function randomSkulls(data){
     $('.brasao').html('<img class="logo" src="assets/images/skulls/fury-skull-'+data[Math.floor(Math.random() * data.length)]+'.png" alt="Skull Fury of the Killers">');
