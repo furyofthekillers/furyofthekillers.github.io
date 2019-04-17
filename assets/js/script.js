6095be33504e0cea50f53b87128eb93a01d98e67
Herson Schluter <herson.schluter@telefonica.com>
2019-04-17 13:15:26 -0300

    Branch de desenvolvimento do site completo

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 function carregarMembros(){
     $.ajax({
-        type: "GET", 
+        type: "GET",
         url: "assets/json/members.json",
         timeout: 3000,
         datatype: 'JSON',
@@ -25,7 +25,7 @@ function carregarMembros(){
                 _listMembers.push(member);
             });
             listSkulls(_listMembers);
-            // lineMembers(_listMembers);
+            lineMembers(_listMembers);
             return _listMembers;
         }
     });
@@ -37,17 +37,8 @@ function listSkulls(data){
     return _skulls;
 }
 function lineMembers(data){
-    var lineComp = ".competitive";
-    var lineCasu = ".casual";
     $.each(data, function( i, member ) {
-        if(member.line == 'competitive'){
-            $(lineComp).append('<li><h2>'+member.gametag+'</h2><br><span>'+member.name+'</span><br><span>'+member.date+'</span></li>');
-        }
-    });
-    $.each(data, function( i, member ) {
-        if(member.line == 'casual'){
-            $(lineCasu).append('<li><h2>'+member.gametag+'</h2><br><span>'+member.name+'</span><br><span>'+member.date+'</span></li>');
-        }
+        $('.members').append('<li class="center-xs col-lg-3 col-md-3 col-sm-6 col-xs-12"><div class="member"><span class="skulls"><img src="assets/images/skulls/fury-skull-'+member.skull+'.png" alt="" class="skull"></span><div class="data start-xs"><h4 class="gametag">'+member.gametag+'</h4><p class="name">'+member.name+'</p><p class="date">'+member.date+'</p><p class="patent">'+member.patent+'</p></div></div></li>');
     });
 }
 function randomSkulls(data){
